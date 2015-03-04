e5dd2d369d823a6ab4f0dcc4c283c7baeab6a24c
Felix Obenauer <f.obenauer@gmail.com>
2015-03-04 21:58:36 +0100

    Add and rework some comments

--- plot2.R
+++ plot2.R
@@ -1,16 +1,18 @@
-##The Data file is assumed to be in the parent directory of this file.
+#The Data file is assumed to be in the parent directory of this file.
 fileLoc <- "../household_power_consumption.txt"
-##Read only the first line, just to get the header information
+#Read only the first line, just to get the header information
 headData <- read.table(fileLoc, sep = ";", nrows = 1, header = TRUE)
-##Read the relevant lines from the file.
-##They have been determined with grep to (dramatically) decrease loading times.
+#Read the relevant lines from the file.
+#They have been determined with grep to (dramatically) decrease loading times.
 realData <- read.table(fileLoc, sep = ";", skip = 66637, nrows = 2880, header = FALSE)
-##Assign the variable names to the real data
+#Assign the variable names to the real data
 names(realData) <- names(headData)
-## Create Date objects from first two columns
+#Create Date objects from first two columns
 dates <- strptime(paste(realData[,1], realData[,2]), format = "%d/%m/%Y %H:%M:%S")
 
+#Create a 'png' graphics device, specifying the filename and image dimensions
 png(filename = "plot2.png", width = 480, height = 480)
 plot(dates, realData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+#Close the file device
 dev.off()
 
